b108f2d5112a84301b4ff35b840a520eda3c0fdd
MarcosArmando <narvaezaldana@gmail.com>
2023-01-20 12:56:11 -0600

    modify mainActivity name

--- app/src/main/java/com/marcosnarvaez/android/testshoppingapp/LogInActivity.kt
+++ app/src/main/java/com/marcosnarvaez/android/testshoppingapp/LogInActivity.kt
@@ -3,7 +3,7 @@ package com.marcosnarvaez.android.testshoppingapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LogInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
