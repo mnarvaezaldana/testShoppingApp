84e7850e160f4927b5de216f4e93dd0f3829c951
MarcosArmando <narvaezaldana@gmail.com>
2023-01-21 13:03:50 -0600

    updata compileSDK level

--- app/src/main/java/com/marcosnarvaez/android/testshoppingapp/networking/ProductsListResponseSchema.kt
+++
@@ -1,5 +0,0 @@
-package com.marcosnarvaez.android.testshoppingapp.networking
-
-import com.marcosnarvaez.android.testshoppingapp.products.Product
-
-data class ProductsListResponseSchema (val products: List<Product>)
