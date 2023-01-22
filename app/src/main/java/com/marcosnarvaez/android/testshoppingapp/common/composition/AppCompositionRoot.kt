62d392144aa86b9a391e7eb201afd5f51e7d7105
MarcosArmando <narvaezaldana@gmail.com>
2023-01-22 01:30:54 -0600

    rely on lazy initialization for services

--- app/src/main/java/com/marcosnarvaez/android/testshoppingapp/common/composition/AppCompositionRoot.kt
+++ app/src/main/java/com/marcosnarvaez/android/testshoppingapp/common/composition/AppCompositionRoot.kt
@@ -10,21 +10,25 @@ import java.util.concurrent.TimeUnit
 
 class AppCompositionRoot {
 
-    private fun okHttpClient(): OkHttpClient {
-        return OkHttpClient.Builder()
+    private val okHttpClient: OkHttpClient by lazy {
+        OkHttpClient.Builder()
             .connectTimeout(30, TimeUnit.SECONDS)
             .readTimeout(30, TimeUnit.SECONDS)
             .writeTimeout(30, TimeUnit.SECONDS)
             .build()
     }
 
-    private val retrofit: Retrofit = Retrofit.Builder()
-        .baseUrl("https://fakestoreapi.com/")
-        .addConverterFactory(GsonConverterFactory.create())
-        .client(okHttpClient())
-        .build()
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl("https://fakestoreapi.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+    }
 
-    private val storeApi: StoreApi = retrofit.create(StoreApi::class.java)
+    private val storeApi: StoreApi by lazy {
+        retrofit.create(StoreApi::class.java)
+    }
 
     val fetchProductsUseCase get() = FetchProductsUseCase(storeApi)
 
