3e970f14dda7e28c94bc2d0f720ed1763dea749d
MarcosArmando <narvaezaldana@gmail.com>
2023-01-24 13:55:41 -0600

    change details from UI

--- app/src/main/java/com/marcosnarvaez/android/testshoppingapp/views/imageloader/ImageLoader.kt
+++ app/src/main/java/com/marcosnarvaez/android/testshoppingapp/views/imageloader/ImageLoader.kt
@@ -3,13 +3,13 @@ package com.marcosnarvaez.android.testshoppingapp.views.imageloader
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
+import com.marcosnarvaez.android.testshoppingapp.R
 import javax.inject.Inject
 
 class ImageLoader @Inject constructor(private val activity: AppCompatActivity) {
 
 
     fun loadImage(imageUrl: String, target: ImageView) {
-        Glide.with(activity).load(imageUrl).into(target)
+        Glide.with(activity).load(imageUrl).placeholder(R.drawable.logo).into(target)
     }
 }
